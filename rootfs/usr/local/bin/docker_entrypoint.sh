fe02ccfd3a7bd14bd79a9241ff6ea0759e901a84
stlouisn <stlouisn@hotmail.com>
2019-03-22 11:27:45 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -3,7 +3,7 @@
 #=========================================================================================
 
 # Fix user and group ownerships for '/config'
-chown -R sonarr:sonarr /config
+chown -R www-data:www-data /config
 
 # Delete pid if it exists
 [[ -e /config/nzbdrone.pid ]] && rm -f /config/nzbdrone.pid
