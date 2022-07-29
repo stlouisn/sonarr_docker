ece809b82b30445339863764aa950222453e485e
stlouisn <stlouisn@hotmail.com>
2022-07-29 20:05:19 -0300

    Create docker_entrypoint.sh

---
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#=========================================================================================
+
+# Fix user and group ownerships for '/config'
+chown -R sonarr:sonarr /config
+
+# Delete PID if it exists
+if
+    [ -e "/config/sonarr.pid" ]
+then
+    rm -f /config/sonarr.pid
+fi
+
+#=========================================================================================
+
+# Start sonarr in console mode
+exec gosu sonarr \
+    /usr/bin/mono --debug \
+    /Sonarr/Sonarr.exe -nobrowser -data=/config
+    
