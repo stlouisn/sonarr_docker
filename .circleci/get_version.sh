bfeb696d4a2ebd6d801ff3b7578ca5db20f3209d
stlouisn <stlouisn@hotmail.com>
2022-07-30 10:59:00 -0300

    Delete get_version.sh

--- .circleci/get_version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# Application version
-APP_VERSION="$(curl -sSL --retry 5 --retry-delay 2 "http://services.sonarr.tv/v1/releases" | jq -r '.[].version' | head -n 1)"
-echo "$APP_VERSION"
